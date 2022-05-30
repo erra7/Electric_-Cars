8db03706fdebb5690500f8aefac4ef8b73bbb573
erra7 <98060281+erra7@users.noreply.github.com>
2022-05-30 12:04:22 +0200

    Update web_1.py

--- web_1.py
+++ web_1.py
@@ -91,94 +91,94 @@ top_brands = top_brands_10(cars_1)
 
 st.dataframe(top_brands)
 
-#Displays top 10 Cars 
+# #Displays top 10 Cars 
 
-st.write("""
-### Top 10 Cars
-""")
+# st.write("""
+# ### Top 10 Cars
+# """)
 
 
-def top_cars_10(cars_1 : pd.DataFrame) :
-    return(
-       cars_1
-        .groupby(['Name', 'batter_capacity_kWh', 'top_speed_km_h', 'fast_charge_speed_km_h'])['price_in_euros']
-        .sum()
-        .sort_values(ascending=False)
-        .head(10)
-        .reset_index()
-      )
-top_brands = top_cars_10(cars_1)
+# def top_cars_10(cars_1 : pd.DataFrame) :
+#     return(
+#        cars_1
+#         .groupby(['Name', 'batter_capacity_kWh', 'top_speed_km_h', 'fast_charge_speed_km_h'])['price_in_euros']
+#         .sum()
+#         .sort_values(ascending=False)
+#         .head(10)
+#         .reset_index()
+#       )
+# top_brands = top_cars_10(cars_1)
 
-st.dataframe(top_brands)
+# st.dataframe(top_brands)
 
-# Displays the Price and it features if we give car name  as Input
+# # Displays the Price and it features if we give car name  as Input
 
-st.write("""
-### Choose the Name of the car 
-""")
+# st.write("""
+# ### Choose the Name of the car 
+# """)
 
-name = st.selectbox(
-    ' ',
-     (cars_1['Name'].unique()))
+# name = st.selectbox(
+#     ' ',
+#      (cars_1['Name'].unique()))
 
-def name_of_car(cars_1: pd.DataFrame, Name: str):
+# def name_of_car(cars_1: pd.DataFrame, Name: str):
     
-    data=cars_1.copy()
+#     data=cars_1.copy()
     
-    return(data         
-    .query('Name == @Name')  
-    .filter(['price_in_euros','batter_capacity_kWh', 'acceleration_in_sec', 'TopSpeed', 'fast_charge_speed_km_h', 
-             'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats'])        
-    )
+#     return(data         
+#     .query('Name == @Name')  
+#     .filter(['price_in_euros','batter_capacity_kWh', 'acceleration_in_sec', 'TopSpeed', 'fast_charge_speed_km_h', 
+#              'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats'])        
+#     )
 
-output_1 = name_of_car(cars_1, name)
+# output_1 = name_of_car(cars_1, name)
 
-st.write("""
-### Car Features  
-""")
+# st.write("""
+# ### Car Features  
+# """)
     
-st.dataframe(output_1)
+# st.dataframe(output_1)
 
 
 # To Choose the Price range of a Cars 
 
-st.write("""
-### Choose the Price Range 
-""")
+# st.write("""
+# ### Choose the Price Range 
+# """)
 
-numberFrom,numberTo = st.slider('Select the Price Range', value=[min(cars_1['price_in_euros']),max(cars_1['price_in_euros'])])
-# numberFrom = range[0]
-# numberTo = range[1]
+# numberFrom,numberTo = st.slider('Select the Price Range', value=[min(cars_1['price_in_euros']),max(cars_1['price_in_euros'])])
+# # numberFrom = range[0]
+# # numberTo = range[1]
 
-def price_range(cars_1: pd.DataFrame, from_PriceinGermany: int, to_PriceinGermany: int):
-    data=cars_1.copy()
-    return(data    
-    .query('price_in_euros >= @from_PriceinGermany & price_in_euros <= @to_PriceinGermany')
-     .filter(['Name','batter_capacity_kWh', 'acceleration_in_sec','top_speed_km_h', 'fast_charge_speed_km_h', 
-             'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats', 'price_in_euros']) 
-          )
+# def price_range(cars_1: pd.DataFrame, from_PriceinGermany: int, to_PriceinGermany: int):
+#     data=cars_1.copy()
+#     return(data    
+#     .query('price_in_euros >= @from_PriceinGermany & price_in_euros <= @to_PriceinGermany')
+#      .filter(['Name','batter_capacity_kWh', 'acceleration_in_sec','top_speed_km_h', 'fast_charge_speed_km_h', 
+#              'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats', 'price_in_euros']) 
+#           )
 
-output_2 = price_range(cars_1, numberFrom, numberTo)
-st.dataframe(output_2)
+# output_2 = price_range(cars_1, numberFrom, numberTo)
+# st.dataframe(output_2)
 
 ##To Choose the Range of a Car 
 
-st.write("""
-### Choose the Range of the Car
-""")
+# st.write("""
+# ### Choose the Range of the Car
+# """)
 
-numberFrom,numberTo = st.slider('Select the Range of the Car', value=[min(cars_1['range_km']),max(cars_1['range_km'])])
+# numberFrom,numberTo = st.slider('Select the Range of the Car', value=[min(cars_1['range_km']),max(cars_1['range_km'])])
 
-def range_of_car(cars_1: pd.DataFrame, from_range: int, to_range: int):
+# def range_of_car(cars_1: pd.DataFrame, from_range: int, to_range: int):
     
-    data=cars_1.copy()
+#     data=cars_1.copy()
     
-    return(data    
-    .query('range_km >= @from_range & range_km <= @to_range')
-    .filter(['Name', 'price_in_euros', 'batter_capacity_kWh', 'acceleration_in_sec','top_speed_km_h', 
-             'fast_charge_speed_km_h', 'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats'])        
-          )
+#     return(data    
+#     .query('range_km >= @from_range & range_km <= @to_range')
+#     .filter(['Name', 'price_in_euros', 'batter_capacity_kWh', 'acceleration_in_sec','top_speed_km_h', 
+#              'fast_charge_speed_km_h', 'range_km', 'efficiency_Wh_km', 'Drive', 'NumberofSeats'])        
+#           )
 
 
-output_4 = range_of_car(cars_1, 150,200)
-st.dataframe(output_4)
+# output_4 = range_of_car(cars_1, 150,200)
+# st.dataframe(output_4)
